9272ba88239a044f24aabe32c22a1c334c2c8b75
blablapolicja <blablapolicja@gmail.com>
2014-11-17 16:11:12 +0200

    reindent

--- myApp.js
+++ myApp.js
@@ -17,16 +17,16 @@ angular.module('myApp', [])
 	.controller('mainController', function ($scope) {
 		'use strict';
 		$scope.problemsList = [
-			{id: 1, name: "Wastewater", 	 		tags: ["Water Pollution"]},
-			{id: 2, name: "Street dogs", 	 		tags: ["Other"]},
-			{id: 3, name: "Illegal dumping", 		tags: ["Dump"]},
+			{id: 1, name: "Wastewater",				tags: ["Water Pollution"]},
+			{id: 2, name: "Street dogs",			tags: ["Other"]},
+			{id: 3, name: "Illegal dumping",		tags: ["Dump"]},
 			{id: 4, name: "Beach pollution",		tags: ["Dump", "Water Pollution"]},
-			{id: 5, name: "A lot of dead fish", 		tags: ["Poaching", "Water Pollution"]},
-			{id: 6, name: "Illegal constructions",		tags: ["Other"]},
-			{id: 7, name: "Smelly water", 			tags: ["Water Pollution", "Other", "Dump"]},
-			{id: 8, name: "High level of CO2", 		tags: ["Other"]},
-			{id: 9, name: "An abandoned building", 		tags: ["Other", "Dump"]},
-			{id: 10, name: "Poachers", 			tags: ["Poaching"]}
+			{id: 5, name: "A lot of dead fish",		tags: ["Poaching", "Water Pollution"]},
+			{id: 6, name: "Illegal constructions",	tags: ["Other"]},
+			{id: 7, name: "Smelly water",			tags: ["Water Pollution", "Other", "Dump"]},
+			{id: 8, name: "High level of CO2",		tags: ["Other"]},
+			{id: 9, name: "An abandoned building",	tags: ["Other", "Dump"]},
+			{id: 10, name: "Poachers",				tags: ["Poaching"]}
 		];
 
 		$scope.tags = ["Water Pollution", "Dump", "Poaching", "Other"];//you can add new tag

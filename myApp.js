8239d6a414d20544cb3d56dc44a1da8b8fbb2394
blablapolicja <blablapolicja@gmail.com>
2014-11-17 14:24:14 +0200

    refactor

--- myApp.js
+++ myApp.js
@@ -5,8 +5,8 @@ angular.module('myApp', [])
 			var tempProblems = [];
 			angular.forEach(problemsList, function(problem){
 				angular.forEach(filters, function(filter){
-					var index = problem.type.indexOf(filter.name);
-					if (filter.checked && tempProblems[tempProblems.length - 1] != problem && index >= 0) {
+					var index = problem.tags.indexOf(filter);
+					if (tempProblems[tempProblems.length - 1] != problem && index >= 0) {
 						tempProblems.push(problem);
 					}
 				});
@@ -17,28 +17,22 @@ angular.module('myApp', [])
 	.controller('mainController', function ($scope) {
 		'use strict';
 		$scope.problemsList = [
-			{id: 1, name: "Wastewater", 	 		type: ["Water Pollution"]},
-			{id: 2, name: "Street dogs", 	 		type: ["Other"]},
-			{id: 3, name: "Illegal dumping", 		type: ["Dump"]},
-			{id: 4, name: "Beach pollution",		type: ["Dump", "Water Pollution"]},
-			{id: 5, name: "A lot of dead fish", 	type: ["Poaching", "Water Pollution"]},
-			{id: 6, name: "Illegal constructions",	type: ["Other"]},
-			{id: 7, name: "Smelly water", 			type: ["Water Pollution", "Other"]},
-			{id: 8, name: "High level of CO2", 		type: ["Other"]},
-			{id: 9, name: "An abandoned building", 	type: ["Other", "Dump"]}
+			{id: 1, name: "Wastewater", 	 		tags: ["Water Pollution"]},
+			{id: 2, name: "Street dogs", 	 		tags: ["Other"]},
+			{id: 3, name: "Illegal dumping", 		tags: ["Dump"]},
+			{id: 4, name: "Beach pollution",		tags: ["Dump", "Water Pollution"]},
+			{id: 5, name: "A lot of dead fish", 	tags: ["Poaching", "Water Pollution"]},
+			{id: 6, name: "Illegal constructions",	tags: ["Other"]},
+			{id: 7, name: "Smelly water", 			tags: ["Water Pollution", "Other"]},
+			{id: 8, name: "High level of CO2", 		tags: ["Other"]},
+			{id: 9, name: "An abandoned building", 	tags: ["Other", "Dump"]}
 		];
 
-		$scope.filters = [
-			{name: "Water Pollution", checked: true},
-			{name: "Dump", 			  checked: true},
-			{name: "Poaching", 		  checked: true},
-			{name: "Other", 		  checked: true}
-		];
+		$scope.tags = ["Water Pollution", "Dump", "Poaching", "Other"];//you can add new tag
+		$scope.selectedTags = [];
 
 		$scope.checkAll = function() {
-			angular.forEach($scope.filters, function(filter){
-				filter.checked = true;
-			});
+			angular.copy($scope.tags, $scope.selectedTags);
 		};
 
 		//Add new problem
@@ -46,12 +40,12 @@ angular.module('myApp', [])
 		$scope.name = "";
 		$scope.selection = [];
 
-		$scope.toggleSelection = function(type) {
-		    var index = $scope.selection.indexOf(type);
+		$scope.toggleSelection = function(tag, array) {
+		    var index = array.indexOf(tag);
 		    if (index > -1) {
-		    	$scope.selection.splice(index, 1);
+		    	array.splice(index, 1);
 		    } else {
-		    	$scope.selection.push(type);
+		    	array.push(tag);
 		    }
 		};
 
@@ -59,7 +53,8 @@ angular.module('myApp', [])
 			var newProblem = {
 				id: $scope.problemsList.length + 1, 
 				name: $scope.name, 
-				type: $scope.selection};
+				type: $scope.selection
+			};
 			$scope.problemsList.push(newProblem);
 			$scope.name = "";
 			$scope.selection =[];
